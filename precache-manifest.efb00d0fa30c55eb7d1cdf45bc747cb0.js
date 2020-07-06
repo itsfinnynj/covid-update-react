9021f9013ca623acd0a1d221fdcd13fd099c1320
finnynj <finny.nj@gmail.com>
2020-07-06 20:01:46 +0530

    Updates

---
+++ precache-manifest.efb00d0fa30c55eb7d1cdf45bc747cb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c19b91d029519809164ddef1b5ba46d",
+    "url": "/covid-tracker-react/index.html"
+  },
+  {
+    "revision": "2371aef3227b8ab81aa7",
+    "url": "/covid-tracker-react/static/css/main.3e7ae0ae.chunk.css"
+  },
+  {
+    "revision": "89f60dc0cc92c30bfc91",
+    "url": "/covid-tracker-react/static/js/2.17d8a168.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker-react/static/js/2.17d8a168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2371aef3227b8ab81aa7",
+    "url": "/covid-tracker-react/static/js/main.4accd0e0.chunk.js"
+  },
+  {
+    "revision": "761c9ce0a7b37ebbb246",
+    "url": "/covid-tracker-react/static/js/runtime-main.aa58b133.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker-react/static/media/image.d7265326.png"
+  }
+]);
